0101456864ba246f2ffdfad4cdd3daf3f2d2c9c3
patrick <873948000@qq.com>
2017-04-10 23:33:25 +0800

    Hava a breif look at Vue.js!

---
+++ Vue.js/app/app.js
@@ -0,0 +1,7 @@
+new Vue({
+    el: '#app', // 对应了 id
+    data: {
+        /* 这里的 message 对应了 html 文件中的 message */
+        message: 'Hello, Patrick!'
+    }
+})
---
+++ Vue.js/app2/app2.js
@@ -0,0 +1,6 @@
+var app2 = new Vue({
+    el: 'app-2',
+    data:{
+        message: '现在是' + new Date() + ', and patrick is missing U.'
+    }
+})
