90d66ef41a56d94c6bc5e19f8fabd7cc26b44328
patrick <873948000@qq.com>
2017-04-14 23:29:58 +0800

    Follow the guide of Vue.js.

---
+++ Vue.js/app4/app4.js
@@ -0,0 +1,11 @@
+var app4 = new Vue
+({
+    el: '#app-4',
+    data:
+    [
+        { text: '学习算法导论' },
+        { text: '学习 Vue' },
+        { text: '写 ACM 作业' },
+        { text: '写计组作业' }
+    ]
+})
